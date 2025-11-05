e7f56061bf8f0ac5b90925c95e55933f28c82a3f
Kim-Yukyung <yukyung116@gmail.com>
2025-11-05 21:36:25 +0900

    ♻️ 불필요한 변수/로직 제거

--- PROJECT01/main.go
+++ PROJECT01/main.go
@@ -16,9 +16,9 @@ func main() {
 	target := rng.Intn(100)
 
 	var number int
-	cnt, flag := 0, false
+	cnt := 1
 
-	for flag != true {
+	for {
 		fmt.Print("숫자값을 입력하세요: ")
 
 		// 사용자 입력
@@ -32,10 +32,9 @@ func main() {
 		} else if number > target {
 			fmt.Println("입력하신 숫자가 더 큽니다.")
 		} else {
-			fmt.Print("숫자를 맞췄습니다. 축하합니다. ")
-			flag = true
+			fmt.Println("숫자를 맞췄습니다. 축하합니다. 시도한 횟수:", cnt)
+			break
 		}
 		cnt++
 	}
-	fmt.Println("시도한 횟수:", cnt)
 }
