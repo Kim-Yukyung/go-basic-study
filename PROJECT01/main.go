0788ff13e513ba42b35c158f12720fa6ce4d6ff6
Kim-Yukyung <yukyung116@gmail.com>
2025-11-05 21:30:02 +0900

    ✨ 랜덤 숫자 생성 방식 변경

--- PROJECT01/main.go
+++ PROJECT01/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	result := (int)(rand.Float64() * 20)
+	//// 0~99 랜덤 숫자 반환
+	//target := (int)(rand.Float64() * 100)
+
+	//rand.Seed(time.Now().UnixNano()) // deprecated
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	target := rng.Intn(100)
 
 	var number int
 	cnt, flag := 0, false
@@ -20,9 +27,9 @@ func main() {
 			return
 		}
 
-		if number < result {
+		if number < target {
 			fmt.Println("입력하신 숫자가 더 작습니다.")
-		} else if number > result {
+		} else if number > target {
 			fmt.Println("입력하신 숫자가 더 큽니다.")
 		} else {
 			fmt.Print("숫자를 맞췄습니다. 축하합니다. ")
@@ -30,5 +37,5 @@ func main() {
 		}
 		cnt++
 	}
-	fmt.Println("시도한 횟수: ", cnt)
+	fmt.Println("시도한 횟수:", cnt)
 }
